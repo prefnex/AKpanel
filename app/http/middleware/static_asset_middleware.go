b528882064bde617b058dd7d57a60e695f457ba4
Ahmed Elkady <elkadwww@gmail.com>
2026-08-22 06:26:20 +0300

    Fix ERR_CONTENT_DECODING_FAILED on panel static assets.
    Remove broken on-the-fly gzip from static middleware; it set Content-Encoding without aborting the handler chain, so browsers received plain JS with a gzip header.

--- app/http/middleware/static_asset_middleware.go
+++ app/http/middleware/static_asset_middleware.go
@@ -1,10 +1,6 @@
 package middleware
 
 import (
-	"compress/gzip"
-	"io"
-	"os"
-	"path/filepath"
 	"strings"
 
 	goravelhttp "github.com/goravel/framework/contracts/http"
@@ -22,59 +18,8 @@ func (m *StaticAssetMiddleware) Signature() string {
 
 func (m *StaticAssetMiddleware) Handle(ctx goravelhttp.Context) {
 	path := "/" + strings.TrimPrefix(ctx.Request().Path(), "/")
-	if !strings.HasPrefix(path, "/public/build/") {
-		ctx.Request().Next()
-		return
+	if strings.HasPrefix(path, "/public/build/") {
+		ctx.Response().Header("Cache-Control", "public, max-age=604800, immutable")
 	}
-
-	ctx.Response().Header("Cache-Control", "public, max-age=604800, immutable")
-
-	rel := strings.TrimPrefix(path, "/public/")
-	diskPath := filepath.Join("public", rel)
-	info, err := os.Stat(diskPath)
-	if err != nil || info.IsDir() {
-		ctx.Request().Next()
-		return
-	}
-
-	accept := ctx.Request().Header("Accept-Encoding", "")
-	if !strings.Contains(accept, "gzip") {
-		ctx.Request().Next()
-		return
-	}
-
-	ext := strings.ToLower(filepath.Ext(diskPath))
-	if ext != ".js" && ext != ".css" {
-		ctx.Request().Next()
-		return
-	}
-
-	f, err := os.Open(diskPath)
-	if err != nil {
-		ctx.Request().Next()
-		return
-	}
-	defer f.Close()
-
-	ctx.Response().Header("Content-Encoding", "gzip")
-	ctx.Response().Header("Vary", "Accept-Encoding")
-	if ext == ".css" {
-		ctx.Response().Header("Content-Type", "text/css; charset=utf-8")
-	} else {
-		ctx.Response().Header("Content-Type", "application/javascript")
-	}
-	_ = ctx.Response().Status(200).Stream(func(w goravelhttp.StreamWriter) error {
-		gz := gzip.NewWriter(streamWriterAdapter{w: w})
-		defer gz.Close()
-		_, err := io.Copy(gz, f)
-		return err
-	})
-}
-
-type streamWriterAdapter struct {
-	w goravelhttp.StreamWriter
-}
-
-func (a streamWriterAdapter) Write(p []byte) (int, error) {
-	return a.w.Write(p)
+	ctx.Request().Next()
 }
